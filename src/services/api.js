1c2a9fff398c685fa8aa45e9b92ae76369b768dc
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-24 15:34:09 -0300

    Initial Commit + Req. 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categoriesJSON = await fetchCategories.json();
+  return categoriesJSON;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // const fetchCategoryId = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  // const fetchQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const fetchCategoryPerQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  const categoryPerQueryJSON = await fetchCategoryPerQuery.json();
+  return categoryPerQueryJSON;
 }
