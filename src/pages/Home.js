439b7e6a9d237fc7f5d07b09b1313f4a903a7aac
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-05-24 16:23:56 -0300

    main-group-2 requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
@@ -8,6 +9,9 @@ class Home extends React.Component {
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          <button type="button">Carrinho</button>
+        </Link>
       </div>
     );
   }
