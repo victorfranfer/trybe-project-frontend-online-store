6599643cbb2caa79a9be1883cf0df3fef065e7a4
Luciano Dutra Cavalcante de Souza <lucianoo_dutra@hotmail.com>
2022-05-27 16:35:57 -0300

    requesito 10 feito

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -9,6 +9,61 @@ class ShoppingCart extends React.Component {
   componentDidMount() {
     const { products } = this.props;
     this.setState({ listProducts: products });
+    console.log(products);
+  }
+
+  increaseQuantity = ({ target: { name } }) => {
+    const productInfo = {
+      productName: name,
+      quantity: 1,
+      unitProduct: 0,
+    };
+    const { listProducts } = this.state;
+    console.log(listProducts);
+    this.setState((prevState) => ({
+      listProducts: prevState.listProducts.map((product) => {
+        if (product.productName === productInfo.productName) {
+          console.log(typeof product.unitProduct);
+          console.log(typeof product.productPrice);
+          const newProductPrice = (parseFloat(product.productPrice)
+           + product.unitProduct).toFixed(2);
+          return {
+            productName: product.productName,
+            productPrice: newProductPrice,
+            quantity: product.quantity + 1,
+            unitProduct: product.unitProduct,
+          };
+        }
+        return product;
+      }),
+    }));
+  };
+
+  decreaseQuantity = ({ target: { name } }) => {
+    const productInfo = {
+      productName: name,
+      quantity: 1,
+      unitProduct: 0,
+    };
+    const { listProducts } = this.state;
+    console.log(listProducts);
+    this.setState((prevState) => ({
+      listProducts: prevState.listProducts.map((product) => {
+        if (product.productName === productInfo.productName) {
+          console.log(typeof product.unitProduct);
+          console.log(typeof product.productPrice);
+          const newProductPrice = (parseFloat(product.productPrice)
+           - product.unitProduct).toFixed(2);
+          return {
+            productName: product.productName,
+            productPrice: newProductPrice,
+            quantity: product.quantity - 1,
+            unitProduct: product.unitProduct,
+          };
+        }
+        return product;
+      }),
+    }));
   }
 
   render() {
@@ -18,8 +73,31 @@ class ShoppingCart extends React.Component {
         ? listProducts.map((product) => (
           <div key={ product.productName }>
             <span data-testid="shopping-cart-product-name">{product.productName}</span>
+            <button
+              data-testid="product-decrease-quantity"
+              type="button"
+              name={ product.productName }
+              id={ product.productPrice }
+              onClick={ this.decreaseQuantity }
+              disabled={ product.quantity <= 1 }
+            >
+              -
+            </button>
+            <span
+              data-testid="shopping-cart-product-quantity"
+            >
+              { product.quantity }
+            </span>
+            <button
+              data-testid="product-increase-quantity"
+              type="button"
+              name={ product.productName }
+              id={ product.productPrice }
+              onClick={ this.increaseQuantity }
+            >
+              +
+            </button>
             <span>{product.productPrice}</span>
-            <span data-testid="shopping-cart-product-quantity">1</span>
           </div>
         )) : (
           <div>
